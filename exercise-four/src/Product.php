249f2a0498730c52789011239de62ec8b3ae15a5
Keys <keys14052002@gmail.com>
2025-06-10 02:01:01 +0000

    Add Product class in exercise four

---
+++ exercise-four/src/Product.php
@@ -0,0 +1,38 @@
+<?php
+    declare(strict_types=1);
+
+    class Product
+    {
+        public function __construct(
+            private string $id,
+            private string $description,
+            private string $price
+        ) {}
+
+        # Getters
+        public function getId(): string {
+            return $this->id;
+        }
+
+        public function getDescription(): string {
+            return $this->description;
+        }
+
+        public function getPrice(): string {
+            return $this->price;
+        }
+
+        # Setters
+        public function setId(string $id): void {
+            $this->id = $id;
+        }
+
+        public function setDescription(string $description): void {
+            $this->description = $description;
+        }
+
+        public function setPrice(string $price): void {
+            $this->price = $price;
+        }
+        
+    }
